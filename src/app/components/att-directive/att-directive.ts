8e5da59b02885d06dd68a3ce135be4c0dee56c45
harsh170699 <harsh.shah11@einfochips.com>
2026-05-04 12:14:23 +0530

    8. Pipes and Lifecycle Hooks in Angular - added built-in pipes (uppercase, lowercase, titlecase, slice, json, date) and implemented component lifecycle hooks

--- src/app/components/att-directive/att-directive.ts
+++ src/app/components/att-directive/att-directive.ts
@@ -1,16 +1,63 @@
-import { Component, signal } from '@angular/core';
-import { NgClass, NgStyle } from '@angular/common';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { DatePipe, JsonPipe, LowerCasePipe, NgClass, NgStyle, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-att-directive',
-  imports: [NgClass, NgStyle],
+  imports: [NgClass, NgStyle, UpperCasePipe, LowerCasePipe, TitleCasePipe, SlicePipe, JsonPipe, DatePipe],
   templateUrl: './att-directive.html',
   styleUrl: './att-directive.css',
 })
-export class AttDirective {
+export class AttDirective implements OnInit, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   div1ClassName = signal<string>('');
 
   isDiv2Green: boolean = false;
+
+  firstName: string = 'Harsh';
+  courseName: string = 'Angular 20 Practice';
+
+  rollNoList = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+  studentObj: any = {
+    name: 'Harsh',
+    city: 'Pune',
+    state: 'Gujarat'
+  }
+
+  currentDate: Date = new Date();
+
+  constructor() {
+    console.log('AttDirective constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('AttDirective ngOnInit');
+    // To Trigger API calls
+    // To subscribe to observables
+  }
+  
+  ngAfterContentInit(): void {
+    console.log('AttDirective ngAfterContentInit');
+  }
+  
+  ngAfterContentChecked(): void {
+    console.log('AttDirective ngAfterContentChecked');
+  }
+  
+  ngAfterViewInit(): void {
+    console.log('AttDirective ngAfterViewInit');
+    // To access DOM elements
+    // To initialize Child components
+  }
+  
+  ngAfterViewChecked(): void {
+    console.log('AttDirective ngAfterViewChecked');
+  }
+  
+  ngOnDestroy(): void {
+    console.log('AttDirective ngOnDestroy');
+    // To cleanup resources
+    // To unsubscribe from observables
+  }
 
   setBgClass(className: string) {
     this.div1ClassName.set(className);
